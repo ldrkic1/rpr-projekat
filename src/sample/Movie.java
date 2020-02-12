41e5e588e4a78b43fae78cb9641526b4b7ae7cde
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-02-12 23:32:25 +0100

    Attribute year is added in Movie class and User is created.

--- src/sample/Movie.java
+++ src/sample/Movie.java
@@ -5,18 +5,29 @@ import java.util.ArrayList;
 public class Movie {
     private String title;
     private String genre;
+    private int year;
     private String director;
     private ArrayList<String> mainActors = new ArrayList<>();
     private String description;
     private int durationMinutes;
 
-    public Movie(String title, String genre, String director, ArrayList<String> mainActors, String description, int durationMinutes) {
+    public Movie() { }
+    public Movie(String title, String genre, int year, String director, ArrayList<String> mainActors, String description, int durationMinutes) {
         this.title = title;
         this.genre = genre;
+        this.year = year;
         this.director = director;
         this.mainActors = mainActors;
         this.description = description;
         this.durationMinutes = durationMinutes;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public String getTitle() {
