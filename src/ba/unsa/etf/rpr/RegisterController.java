36a60986c2c81aed7d3990e9f237004f701fc320
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-08-01 18:43:14 +0200

    gui for registration added

--- src/ba/unsa/etf/rpr/RegisterController.java
+++ src/ba/unsa/etf/rpr/RegisterController.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
 public class RegisterController {
+    public Button registerButton;
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public PasswordField repeatPasswordField;
+
+    public void  registerAction(ActionEvent actionEvent) {
+        System.out.println("klik");
+    }
 }
