7cee218d82862b885107b06152c64d84ce093ded
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-08-01 17:16:54 +0200

    User class changed

--- src/ba/unsa/etf/rpr/User.java
+++ src/ba/unsa/etf/rpr/User.java
@@ -1,49 +1,70 @@
 package ba.unsa.etf.rpr;
 
 public class User {
-    private String name;
-    private String phoneNumber;
-    private String email;
-    private String address;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private int roomNumber;
 
-    public User() {}
-
-    public User(String name, String phoneNumber, String email, String address) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
+    public User() {
     }
 
-    public String getName() {
-        return name;
+    public User(int id, String firstName, String lastName, String username, String password, int roomNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.roomNumber = roomNumber;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getId() {
+        return id;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getEmail() {
-        return email;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getLastName() {
+        return lastName;
     }
 
-    public String getAddress() {
-        return address;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
     }
 }
