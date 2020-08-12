0ebdb1ac7539ccc59acbba20ac2fc8afbbff8fb0
ldrkic1 <ldrkic1@etf.unsa.ba>
2020-08-12 16:36:08 +0200

    generisanje lozinke za gosta hotela

--- src/ba/unsa/etf/rpr/AddHotelGuestController.java
+++ src/ba/unsa/etf/rpr/AddHotelGuestController.java
@@ -1,43 +1,64 @@
 package ba.unsa.etf.rpr;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 
 import java.io.IOException;
-
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+import java.security.SecureRandom;
+import java.util.ArrayList;
 
 public class AddHotelGuestController {
-    public TextField nameField;
-    public TextField surnameField;
     public ChoiceBox usernameChoice;
     public TextField passwordField;
-    public ChoiceBox privilegeChoice;
+    public ChoiceBox<String> privilegeChoice;
     public Button generatePasswordButton;
     public Button cancelButton;
     public Button addButton;
     private VideoLibraryDAO dao = null;
-
+    private TableView<User> tableView = null;
+    private ObservableList<User> list = null;
     public AddHotelGuestController() {
         dao = VideoLibraryDAO.getInstance();
 
     }
     public AddHotelGuestController(TableView<User> usersTableView, ObservableList<User> usersList) throws IOException {
         dao = VideoLibraryDAO.getInstance();
-
+        tableView = usersTableView;
+        list = usersList;
     }
 
     @FXML
     public void initialize() throws IOException {
-
+        int roomsNumber = dao.getRoomsNumber();
+        ArrayList<Integer> list = new ArrayList<>();
+        for( int i = 1; i < roomsNumber; i++) {
+            list.add(i);
+        }
+        ObservableList<Integer> choiceRoomsNumberList = FXCollections.observableArrayList(list);
+        usernameChoice.setItems(choiceRoomsNumberList);
+        usernameChoice.getSelectionModel().selectFirst();
+        ArrayList<String> listPrivilege = new ArrayList<>();
+        listPrivilege.add("DA");
+        listPrivilege.add("NE");
+        ObservableList<String> choicePrivilege = FXCollections.observableArrayList(listPrivilege);
+        privilegeChoice.setItems(choicePrivilege);
+        privilegeChoice.getSelectionModel().selectFirst();
     }
     public void generatePasswordAction(ActionEvent actionEvent) {
+        // ASCII range - alphanumeric (0-9, a-z, A-Z)
+        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+        // each iteration of loop choose a character randomly from the given ASCII range
+        // and append it to StringBuilder instance
+        for (int i = 0; i < 10; i++) {
+            int randomIndex = random.nextInt(chars.length());
+            sb.append(chars.charAt(randomIndex));
+        }
+        passwordField.setText(sb.toString());
     }
 
     public void cancelAction(ActionEvent actionEvent) {
